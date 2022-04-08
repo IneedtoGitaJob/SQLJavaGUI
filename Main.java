c64d4ae4f66207c6196497209c1043c872af7b9b
IneedtoGitaJob <84819013+IneedtoGitaJob@users.noreply.github.com>
2022-04-08 00:51:52 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+//Driver class
+public class Main {
+    public static void main(String[] args) {
+
+
+        //create the Jframe and set default settings
+
+        GUI gui = new GUI();
+
+
+
+    }
+}
